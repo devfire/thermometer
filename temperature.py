87e07b5aaca997b5e4e0b8a0fe43437c35def880
Igor Kantor <igor@devfire.io>
2019-07-20 15:15:44 -0400

    success sending data to adafruit.io

--- temperature.py
+++ temperature.py
@@ -1,9 +1,27 @@
 import glob
 import time
+import sys, os
 
+# Import Adafruit IO REST client.
+from Adafruit_IO import Client, RequestError, Feed
+
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure not to publish it when you publish this code!
+ADAFRUIT_IO_KEY = ''
+
+# Set to your Adafruit IO username.
+# (go to https://accounts.adafruit.com to find your username)
+ADAFRUIT_IO_USERNAME = ''
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+############### sensor setup #######################
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+###################################################
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -24,6 +42,16 @@ def read_temp():
         #return temp_c, temp_f
         return temp_f
 
+#Setup the feed
+try:
+    temperature = aio.feeds('temperature')
+except RequestError:
+    feed = Feed(name="temperature")
+    temperature = aio.create_feed(feed)
+
+#Start sending values
 while True:
-    print ("Temp:",read_temp())
+    current_temp = read_temp()
+    print ("Sending temp:",current_temp)
+    aio.send_data(temperature.key,current_temp) 
     time.sleep(1)
